d346bbfa50f9809b4a1ad82339cf842b1558705c
Neal Clark <nclark@thrownproject.com>
2012-11-15 22:29:23 -0800

    add data bag solo config

--- config/solo.rb
+++ config/solo.rb
@@ -1,3 +1,3 @@
 file_cache_path File.expand_path(File.dirname(__FILE__) + '/../tmp/cache')
 cookbook_path   File.expand_path(File.dirname(__FILE__) + '/../cookbooks')
-
+data_bag_path   File.expand_path(File.dirname(__FILE__) + '/../data_bags')
