aeceda8c4fcacb67ff124a115eb3c739e2f8f745
Neal Clark <nclark@thrownproject.com>
2013-02-21 17:28:45 -0800

    add config for local file backup & cache paths

--- config/solo.rb
+++ config/solo.rb
@@ -1,3 +1,7 @@
 file_cache_path File.expand_path(File.dirname(__FILE__) + '/../tmp/cache')
 cookbook_path   File.expand_path(File.dirname(__FILE__) + '/../cookbooks')
 data_bag_path   File.expand_path(File.dirname(__FILE__) + '/../data_bags')
+
+file_backup_path File.expand_path(File.dirname(__FILE__) + '/../tmp/backup')
+
+cache_options :path => File.expand_path(File.dirname(__FILE__) + '/../tmp/checksums'), :skip_expires => true
