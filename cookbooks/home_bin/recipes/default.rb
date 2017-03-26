fa13a6a023d170ee50d1b431fc41da12421448db
Neal Clark <nclark@thrownproject.com>
2017-03-25 23:37:49 -0700

    BLORG

--- cookbooks/home_bin/recipes/default.rb
+++ cookbooks/home_bin/recipes/default.rb
@@ -4,7 +4,7 @@ end
 
 commands = %w(bssh git-archive-branch git-cor git-create-archive git-fuckit) +
            %w(git-gist-diff git-pop git-psh git-reset-branch git-reset-remote-branch) + 
-           %w(git-tracker-branch git-x git-force-amend git-rally-branch git-ag) + 
+           %w(git-tracker-branch git-x git-force-amend git-rally-branch) + 
            %w(mk_remote_repo pom tcd)
 
 commands.each do |command_name|
